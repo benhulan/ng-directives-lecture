49c34b2ff01b89f37633879b15197fade5cfeb3f
Ben Hulan <ben.hulan@generalassemb.ly>
2016-03-21 09:47:37 -0700

    fix name

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function(angular) {
   'use strict';
-angular.module('githubApp', [])
+angular.module('GABook', [])
   .controller('Controller', ['$scope', function($scope) {
     $scope.instructors = [
    {
